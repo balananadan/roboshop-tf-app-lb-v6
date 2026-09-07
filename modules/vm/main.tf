18535cf3209a03eee3ce2de3efdd86d400d71a66
bala <balanandan403@gmail.com>
2026-09-07 22:37:08 +0530

    commit

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_network_security_group" "main" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "Any"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
