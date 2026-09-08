3faf09d871b12febeac17811c9862efbbb0bec17
bala <balanandan403@gmail.com>
2026-09-08 14:32:19 +0530

    commit

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "${var.component_name}-${var.env}-nic${count.index}"
-    subnet_id                     = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-1/subnets/snet-denmarkeast-1"
+    subnet_id                     = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-2"
     private_ip_address_allocation = "Dynamic"
   }
 }
