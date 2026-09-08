f11ac7244337ddebc3c3931605b70a2ab2686c13
bala <balanandan403@gmail.com>
2026-09-08 14:41:46 +0530

    commit

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "${var.component_name}-${var.env}-nic${count.index}"
-    subnet_id                     = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-1/subnets/snet-denmarkeast-1"
+    subnet_id                     = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-2/subnets/snet-denmarkeast-1"
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -57,7 +57,7 @@ resource "azurerm_lb" "main" {
   frontend_ip_configuration {
     name                          = "${var.component_name}-${var.env}"
     private_ip_address_allocation = var.lb_type == "private" ? "Dynamic" : null
-    subnet_id                     = var.lb_type == "private" ? "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-1/subnets/snet-denmarkeast-1" : null
+    subnet_id                     = var.lb_type == "private" ? "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-2/subnets/snet-denmarkeast-1" : null
     public_ip_address_id          = var.lb_type == "public" ? azurerm_public_ip.main[0].id : null
   }
 
@@ -74,7 +74,7 @@ resource "azurerm_lb_backend_address_pool_address" "main" {
   name                    = "${var.component_name}-${var.env}-${count.index}"
   backend_address_pool_id = azurerm_lb_backend_address_pool.main[0].id
   ip_address              = azurerm_network_interface.main[count.index].private_ip_address
-  virtual_network_id      = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-1/subnets/snet-denmarkeast-1"
+  virtual_network_id      = "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-2/subnets/snet-denmarkeast-1"
   # "/subscriptions/67d6c4c6-913c-4f47-b3e1-eab7b50d229d/resourceGroups/Nothing/providers/Microsoft.Network/virtualNetworks/vnet-denmarkeast-2"
 
 }
